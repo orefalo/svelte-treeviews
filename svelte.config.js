c5e54434d4c6194d6095eec66ca13dcb5f8d9f3c
Olivier Refalo <orefalo@snapcore.com>
2023-06-02 00:09:16 +0400

    refresh the project

--- svelte.config.js
+++ svelte.config.js
@@ -1,36 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
-import path from 'path';
-
-const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		prerender: {
-			default: true
-		},
 		adapter: adapter({
 			pages: 'docs',
 			assets: 'docs',
-			fallback: null
+			fallback: undefined
 		}),
 		paths: {
-			// change below to your repo name
-			base: dev ? '' : '/svelte-splitpanes'
-		},
-		vite: {
-			optimizeDeps: {
-				include: ['highlight.js', 'highlight.js/lib/core']
-			},
-			resolve: {
-				alias: {
-					$comp: path.resolve('./src/comp'),
-					$lib: path.resolve('./src/lib')
-				}
-			}
+			// Usually the base path will be the root (i.e. defaults by kit to the empty "" path since the env var is undefined),
+			//  but on the official documentation build we set this environment
+			//  variable to the base path where we're deploying to.
+			base: process.env.BASE_PATH
 		}
 	}
 };
